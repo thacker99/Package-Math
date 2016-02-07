3867392c5fed1f87daaed3a28acc21831b8f0655
thacker99 <scott.thacker99@gmail.com>
2016-02-07 01:51:35 +0000

    TestFonctions.java: commenter les différentes parties du code

--- TestFonctions.java
+++ TestFonctions.java
@@ -2,14 +2,21 @@ class TestFonctions{
 	
 	public static void main(String[] args){
 		
+		// déclaration des variables 
 		int x = 10;
 		int y = -3;
 		int val;
-		Fonctions f = new Fonctions();
+		Fonctions f = new Fonctions(); // création de l'objet f 
+		
+		// teste et affichage de l'addition
 		val = f.addition(x,y);
 		System.out.println(x + " + " + y + " = " + val);
+		
+		// teste et affichage de la soustraction
 		val = f.soustraction(x,y);
 		System.out.println(x + " - " + y + " = " + val);
+		
+		// calcul et affichage du cube d'un entier
 		System.out.println("cube( + "+ x + ") = " + f.cube(x));
 		
 	}
