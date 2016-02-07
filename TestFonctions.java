889884f4dfc327596a0cdc764ebce65e59d5ef26
thacker99 <scott.thacker99@gmail.com>
2016-02-07 01:44:45 +0000

    TestFonctions.java: déclaration des différents variable et instanciation de la classe

--- TestFonctions.java
+++ TestFonctions.java
@@ -1,5 +1,13 @@
 class TestFonctions{
 	
-	public static void main(String[] args){}
+	public static void main(String[] args){
+		
+		int x = 10;
+		int y = -3;
+		int val;
+		Fonctions f = new Fonctions();
+		
+		
+	}
 	
 }
